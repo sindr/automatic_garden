d7e25f202699ac5edbfa031de998c0fe17c72a0d
Sindre Mohr <sm@geoprovider.no>
2020-06-20 12:04:04 +0200

    bug fix

--- water_valve.ino
+++ water_valve.ino
@@ -1,9 +1,9 @@
 // Automatic garden watering version 1.0
 
-int analogPin = A0;
+int analogPin = A0; // Adding analog pin for reading
 int val = 0;
 bool state = true;
-int on_time = 60000;
+int on_time = 6000;
 
 int night_val = 50;
 int day_val = 150;
@@ -12,7 +12,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // Setup serial
   pinMode(2,OUTPUT);
-  digitalWrite(2,LOW);
+  digitalWrite(2,HIGH); // Sets digital pin to start low
 }
 
 void loop() {
@@ -20,9 +20,9 @@ void loop() {
   val = analogRead(analogPin);
   Serial.println(val); // Used for setting correct values for day/night
   if ((val < night_val) && state){
-      digitalWrite(2,HIGH);
-      delay(on_time);  // keeps valve on for 2 minutes, or 120 seconds...
       digitalWrite(2,LOW);
+      delay(on_time);  // keeps valve on the duration given in on_time
+      digitalWrite(2,HIGH);
       state = false;
     }
 
